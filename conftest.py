35d4850876e9a34c4e983b3b3a73558c40dcbe5e
unknown <aryanalphabin-design>
2025-11-28 17:27:41 +0530

    Added manuall CI

--- conftest.py
+++ conftest.py
@@ -16,6 +16,10 @@ def driver(request):
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-extensions")
     driver = webdriver.Chrome(options=options)
 
     def teardown():
